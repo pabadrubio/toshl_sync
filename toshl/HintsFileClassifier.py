bff3baf73ec80df7b280d41cb17b14ef330ab749
Pablo Abad <pabadrubio@gmail.com>
2018-02-20 23:20:13 +0100

    Add minimum and maximum values to Hints classifier

--- toshl/HintsFileClassifier.py
+++ toshl/HintsFileClassifier.py
@@ -4,6 +4,14 @@
 
 import csv
 
+
+def toFloat(unicodeValue, default):
+    if len(unicodeValue) == 0:
+        return default
+    else:
+        return float(unicodeValue)
+
+
 class HintsFileClassifier:
     def __init__(self, decodingFile):
         self.decoding_hints = self._load_decoding_file(decodingFile)
@@ -15,6 +23,8 @@ class HintsFileClassifier:
             if hint.purpose not in bankEntry.purpose:
                 continue
             if hint.message not in bankEntry.message:
+                continue
+            if -bankEntry.amount < hint.minValue or -bankEntry.amount > hint.maxValue:
                 continue
             return hint.category, hint.tag
 
@@ -32,15 +42,19 @@ class HintsFileClassifier:
                 hint = DecodingHint(unicode(row[0], 'iso-8859-1'),
                                     unicode(row[1], 'iso-8859-1'),
                                     unicode(row[2], 'iso-8859-1'),
-                                    unicode(row[3], 'iso-8859-1'),
-                                    unicode(row[4], 'iso-8859-1'))
+                                    toFloat(unicode(row[3], 'iso-8859-1'), float("-inf")),
+                                    toFloat(unicode(row[4], 'iso-8859-1'), float("+inf")),
+                                    unicode(row[5], 'iso-8859-1'),
+                                    unicode(row[6], 'iso-8859-1'))
                 decoding_hints.append(hint)
         return decoding_hints
 
 class DecodingHint:
-    def __init__(self, account, purpose, message, category, tag):
+    def __init__(self, account, purpose, message, minValue, maxValue, category, tag):
         self.account = account
         self.purpose = purpose
         self.message = message
+        self.minValue = minValue
+        self.maxValue = maxValue
         self.category = category
         self.tag = tag
